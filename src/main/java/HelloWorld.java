74207c90227e57ac83cade42e8d377f401e56f82
“cy” <15764340463@163.com>
2021-09-11 07:27:47 +0800

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @Project: test_idea_github_cy
+ * @Author: chenyang
+ * @Create: 2021-09-11 07:25
+ * @Desc:
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
